2a6acc22d77655e7127d66e4ac79efa136c5b77e
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-03-20 01:27:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./component/Header";
 import About from "./component/About";
 import Projects from "./component/Projects";
 import Testimonial from "./component/Testimonial";
-import Contact from "./component/contact";
+import Contact from "./component/Contact";
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
