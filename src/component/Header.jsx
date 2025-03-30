117c4a6f44128bdc1e951c995e93ea4a3486dacf
Code4fun <145792311+Anupthakurr@users.noreply.github.com>
2025-03-30 18:11:47 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         <div className="space-x-6 mt-16">
           <motion.a
             href="#Project"
-            className="border border-orange bg-orange-700 px-8 py-3 rounded text-black-900 inline-block"
+            className="relative inline-block px-8 py-3 text-white font-semibold rounded-full bg-gradient-to-r from-orange-500 via-red-500 to-yellow-500 transition-transform transform hover:scale-105 hover:shadow-[0_0_10px_rgba(252,211,77,0.8)]"
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
@@ -40,7 +40,7 @@ const Header = () => {
 
           <motion.a
             href="#contact"
-            className="bg-purple-700 px-8 py-3 rounded inline-block"
+            className="relative inline-block px-8 py-3 text-white font-semibold rounded-full bg-gradient-to-r from-purple-500 via-indigo-500 to-pink-500 transition-transform transform hover:scale-105 hover:shadow-[0_0_10px_rgba(192,132,252,0.8)]"
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 1, delay: 0.7, ease: "easeOut" }}
